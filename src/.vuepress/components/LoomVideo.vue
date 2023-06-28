5ccb6f3f0eed0d0e0024552fac8db99666f8e52e
Christoph Krieger <christoph.krieger@anaqor.io>
2023-06-28 16:09:05 +0200

    add vue component for loom videos

---
+++ src/.vuepress/components/LoomVideo.vue
@@ -0,0 +1,40 @@
+<script>
+
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="video-container">
+    <iframe
+        :src="url + '?hide_owner=true&hide_title=true&hideEmbedTopBar=true&hide_share=true'"
+        frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen/>
+  </div>
+</template>
+
+<style scoped>
+
+.video-container {
+  position: relative;
+  border-radius: 10px;
+  padding-bottom: 58.727569331158236%;
+  height: 0;
+}
+
+.video-container iframe {
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
